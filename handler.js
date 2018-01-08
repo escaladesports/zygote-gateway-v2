0a1a7f5c6826b19c84525221d7e9e989009db77d
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-08 15:31:48 -0600

    Successful results

--- handler.js
+++ handler.js
@@ -1,13 +1,17 @@
 import middy from 'middy'
-import { httpErrorHandler, cors } from 'middy/middlewares'
+import { jsonBodyParser, httpErrorHandler, cors } from 'middy/middlewares'
 
 const hello = (event, context, callback) => {
+	console.log('BODY TYPE:', typeof event.body)
+	console.log('BODY:', event.body)
 	callback(null, {
-		result: 'success',
-		message: 'successful callback!'
+		body: JSON.stringify({
+			result: 'success'
+		})
 	})
 }
 
 module.exports.hello = middy(hello)
 	.use(cors())
+	.use(jsonBodyParser())
 	.use(httpErrorHandler())
---
+++ test/place.js
@@ -0,0 +1,20 @@
+'use strict'
+const mochaPlugin = require('serverless-mocha-plugin')
+const expect = mochaPlugin.chai.expect
+const wrapped = mochaPlugin.getWrapper('hello', '/handler.js', 'hello')
+
+describe('Result', () => {
+	it('should be success', () => {
+		return wrapped.run({
+				body: {
+					test: 'abc'
+				}
+			})
+			.then((res) => {
+				expect(res.body).to.not.be.empty
+				res.body = JSON.parse(res.body)
+				expect(res.body.result).to.not.be.empty
+				expect(res.body.result).to.equal('success')
+			})
+	})
+})
