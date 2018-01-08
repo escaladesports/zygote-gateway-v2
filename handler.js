86cda7bbae9912f26d8d0153c8d1b59c288d7ffc
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-08 16:16:26 -0600

    Inline export function

--- handler.js
+++ handler.js
@@ -8,18 +8,15 @@ dotenv.config({
 	path: process.env.NODE_ENV === 'production' ? '.env-production' : '.env'
 })
 
-// Serverless function
-const hello = (event, context, callback) => {
-	console.log('BODY:', event.body)
-	callback(null, {
-		body: JSON.stringify({
-			result: 'success'
+// Export function with middleware
+module.exports.hello = middy((event, context, callback) => {
+		console.log('BODY:', event.body)
+		callback(null, {
+			body: JSON.stringify({
+				result: 'success'
+			})
 		})
 	})
-}
-
-// Export function with middleware
-module.exports.hello = middy(hello)
 	.use(cors())
 	.use(jsonBodyParser())
 	.use(httpErrorHandler())
