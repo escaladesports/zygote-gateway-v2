c31c8c928f0f91a8134555d79d99ce46ab58123a
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-08 16:04:22 -0600

    Environment files

--- handler.js
+++ handler.js
@@ -1,8 +1,13 @@
+require('dotenv').config({
+	silent: true,
+	path: process.env.NODE_ENV === 'production' ? '.env-production' : '.env'
+})
 import middy from 'middy'
 import { jsonBodyParser, httpErrorHandler, cors } from 'middy/middlewares'
 
+console.log('TEST:', process.env.TEST)
+
 const hello = (event, context, callback) => {
-	console.log('BODY TYPE:', typeof event.body)
 	console.log('BODY:', event.body)
 	callback(null, {
 		body: JSON.stringify({
