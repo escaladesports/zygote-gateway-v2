2ec8ac8289c9c5a6e9104776688f8f526a9d14d0
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-12 13:45:46 -0600

    Await in testing

--- test/place.js
+++ test/place.js
@@ -3,17 +3,15 @@ const expect = mochaPlugin.chai.expect
 const wrapped = mochaPlugin.getWrapper('hello', '/handler.js', 'hello')
 
 describe('Result', () => {
-	it('should be success', () => {
-		return wrapped.run({
-				body: {
-					test: 'abc'
-				}
-			})
-			.then((res) => {
-				expect(res.body).to.not.be.empty
-				res.body = JSON.parse(res.body)
-				expect(res.body.result).to.not.be.empty
-				expect(res.body.result).to.equal('success')
-			})
+	it('should be success', async () => {
+		const res = await wrapped.run({
+			body: {
+				test: 'abc'
+			}
+		})
+		expect(res.body).to.not.be.empty
+		res.body = JSON.parse(res.body)
+		expect(res.body.result).to.not.be.empty
+		expect(res.body.result).to.equal('success')
 	})
 })
