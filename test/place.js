87f81a3007d8f5125827500771e6be960d1deb4e
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-12 21:00:41 -0600

    Profile in config

--- test/place.js
+++ test/place.js
@@ -1,14 +1,17 @@
 import mochaPlugin from 'serverless-mocha-plugin'
 const expect = mochaPlugin.chai.expect
-const wrapped = mochaPlugin.getWrapper('hello', '/handler.js', 'hello')
+const wrapped = mochaPlugin.getWrapper('hello', '/handler', 'hello')
 
 describe('Result', () => {
+	it('should always pass', () =>{
+		expect(1).to.equal(1)
+	})
 	it('should be success', async () => {
-		const res = await wrapped.run({
+		const res = await wrapped.run(JSON.stringify({
 			body: {
 				test: 'abc'
 			}
-		})
+		}))
 		expect(res.body).to.not.be.empty
 		res.body = JSON.parse(res.body)
 		expect(res.body.result).to.not.be.empty
