80517312c59778bb6bae975a95caf85aa8909d36
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-18 13:36:09 -0600

    Log cmds

--- utils/deploy.js
+++ utils/deploy.js
@@ -1,4 +1,4 @@
-import cmd from 'exeq'
+import exeq from 'exeq'
 
 let stage = process.env.TRAVIS_BRANCH
 if(stage === 'master'){
@@ -8,16 +8,19 @@ else{
 	stage = stage.replace(/[^0-9\-]/g, '-')
 }
 
-async function deploy(){
+async function cmd(str){
+	console.log(str)
 	try {
-		await cmd(
-			`SLS_DEBUG=* serverless invoke test --stage ${stage} --compilers js:babel-core/register`,
-			`SLS_DEBUG=* serverless deploy --verbose --stage ${stage}`,
-		)
+		await exeq(str)
 	}
-	catch(e){
-		console.error(e)
+	catch(err){
+		console.error(err)
 		process.exit(1)
 	}
+}
+
+async function deploy(){
+	await cmd(`SLS_DEBUG=* serverless invoke test --stage ${stage} --compilers js:babel-core/register`)
+	await cmd(`SLS_DEBUG=* serverless deploy --verbose --stage ${stage}`)
 }
 deploy()
