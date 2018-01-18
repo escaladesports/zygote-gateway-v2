ea43b7a15ee2fb21de90ae431cce8de76737a462
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-18 13:24:29 -0600

    Error test

--- utils/deploy.js
+++ utils/deploy.js
@@ -9,9 +9,15 @@ else{
 }
 
 async function deploy(){
-	await exeq(
-		`SLS_DEBUG=* serverless invoke test --stage ${stage} --compilers js:babel-core/register`,
-		`SLS_DEBUG=* serverless deploy --verbose --stage ${stage}`,
-	)
+	try {
+		await exeq(
+			`SLS_DEBUG=* serverless invoke test --stage ${stage} --compilers js:babel-core/register`,
+			`SLS_DEBUG=* serverless deploy --verbose --stage ${stage}`,
+		)
+	}
+	catch(e){
+		console.error(e)
+		process.exit(1)
+	}
 }
 deploy()
